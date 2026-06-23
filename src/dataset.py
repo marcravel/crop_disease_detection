b2d2ad67d5641421fd227b31c587f84329109650
marcravel <marc.ravelontsalama@gmail.com>
2026-06-23 20:36:52 +0300

    #main train/val/test split size calculated

--- src/dataset.py
+++ src/dataset.py
@@ -13,19 +13,30 @@ DATA_DIR = "data/PlantVillage"
 plant_dataset = datasets.ImageFolder(DATA_DIR, transform)
 
 #Print class names inside the plant_dataset
-for data in plant_dataset.classes:
-    print(data)
-print()
+# for data in plant_dataset.classes:
+#     print(data)
+# print()
 
 #Print mapping
 for key, value in plant_dataset.class_to_idx.items():
     print(f"{key} | idx={value}")
     
-data_loader = torch.utils.data.DataLoader(dataset=plant_dataset,
-                                          batch_size=16,
-                                          shuffle=True,
-                                          num_workers=2)
+# data_loader = torch.utils.data.DataLoader(dataset=plant_dataset,
+#                                           batch_size=16,
+#                                           shuffle=True,
+#                                           num_workers=2)
 
-images, labels = next(iter(data_loader))
-print(f"Images' shape: {images.shape}, labels' shape: {labels.shape}")
+total_len = len(plant_dataset)
+train_size = int(total_len * 0.80)
+val_size = int(total_len * 0.10)
+test_size = total_len - (train_size + val_size)
+
+print("-----------------------")
+print(f"Total_len: {total_len}")
+print(f"Train_size: {train_size}")
+print(f"val_size: {val_size}")
+print(f"test_size: {test_size}")
+print("-----------------------")
+
+print(f"Class numbers: {len(plant_dataset.classes)}")
 
